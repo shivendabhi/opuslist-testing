71b1850d986ca8b8467aa63dd2ccda3eafdda49c
Shiven Dabhi <88247327+shivendabhi@users.noreply.github.com>
2024-10-04 12:33:36 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   reactStrictMode: false,
   webpack(config) {
     // Grab the existing rule that handles SVG imports
