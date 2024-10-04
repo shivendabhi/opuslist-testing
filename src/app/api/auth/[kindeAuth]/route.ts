a8fce82bde1b6cc85fc597131a574d7829c55e7f
Shiven Dabhi <88247327+shivendabhi@users.noreply.github.com>
2024-10-04 11:04:51 -0400

    Update route.ts

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,6 +1,10 @@
-import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextRequest } from "next/server";
-export async function GET(request: NextRequest, {params}:any) {
-    const endpoint = params.kindeAuth;
-    return handleAuth(request, endpoint);
+import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest, { params }: { params: { kindeAuth: string } }) {
+  const endpoint = params.kindeAuth;
+  const authResponse = await handleAuth(request, endpoint);
+  
+  // Convert the auth response to a NextResponse
+  return NextResponse.json(authResponse);
 }
