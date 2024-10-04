84d0a46156d36b0215ecd3bc7962ce83bfad8b63
Shiven Dabhi <88247327+shivendabhi@users.noreply.github.com>
2024-10-04 11:00:31 -0400

    removed testimonials

--- src/app/business/page.tsx
+++ src/app/business/page.tsx
@@ -3,7 +3,6 @@ import Hero from "@/b2bSections/Hero";
 import { LogoTicker } from "@/b2bSections/LogoTicker";
 import { ProductShowcase } from "@/b2bSections/ProductShowcase";
 import Pricing from "@/b2bSections/Pricing";
-import { Testimonials } from "@/b2bSections/Testimonials";
 import { CallToAction } from "@/b2bSections/CallToAction";
 import { Footer } from "@/b2bSections/Footer";
 
