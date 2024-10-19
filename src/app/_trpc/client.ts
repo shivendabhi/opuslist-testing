b1a0ea2912079ee587143062f710e594ed183e43
Shiven Dabhi <88247327+shivendabhi@users.noreply.github.com>
2024-10-18 20:23:58 -0400

    fixing

--- src/app/_trpc/client.ts
+++ src/app/_trpc/client.ts
@@ -1,17 +1,4 @@
 import { AppRouter } from '@/trpc'
 import { createTRPCReact } from '@trpc/react-query'
-import { httpBatchLink } from '@trpc/client'
 
-export const trpc = createTRPCReact<AppRouter>()
-
-export const trpcClient = trpc.createClient({
-  links: [
-    httpBatchLink({
-      url: '/api/trpc',
-      // You can add custom headers here if needed
-      headers: () => {
-        return {}
-      },
-    }),
-  ],
-})
+export const trpc = createTRPCReact<AppRouter>({})
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,7 @@ import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
 import Providers from "@/components/Providers";
-import { trpc, trpcClient } from './_trpc/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React from 'react';
+
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,23 +17,17 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [queryClient] = React.useState(() => new QueryClient());
-
   return (
     <html lang="en" className="relative">
       <Providers>
-        <body
-          className={twMerge(
-            dmSans.className,
-            "antialiased ",
-          )}
-        >
-          <QueryClientProvider client={queryClient}>
-            <trpc.Provider client={trpcClient} queryClient={queryClient}>
-              {children}
-            </trpc.Provider>
-          </QueryClientProvider>
-        </body>
+      <body
+        className={twMerge(
+          dmSans.className,
+          "antialiased ",
+        )}
+      >
+        {children}
+      </body>
       </Providers>
     </html>
   );
