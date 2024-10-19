71a93c5fd3ca04bab41ba7436b108f0d69aca763
Shiven Dabhi <88247327+shivendabhi@users.noreply.github.com>
2024-10-18 20:56:42 -0400

    Update Providers.tsx

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -5,13 +5,19 @@ import { createTRPCClient } from "@trpc/client";
 import { httpBatchLink } from "@trpc/client";
 import {trpc} from "@/app/_trpc/client";
 
+const getBaseUrl = () => {
+  if (typeof window !== "undefined") return ""; // browser should use relative url
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+};
+
 const Providers = ({children}:PropsWithChildren) => {
     const[queryClient] = useState(() => new QueryClient());
     const[trpcClient] = useState(() => 
         trpc.createClient({
             links:[
                 httpBatchLink({
-                    url: "http://localhost:3000/api/trpc",
+                    url: `${getBaseUrl()}/api/trpc`,
                 }),
             ],
         })
