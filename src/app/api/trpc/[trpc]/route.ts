e57d9d0b375a6865cb0dbf2d62f4c1363c2d8622
Shiven Dabhi <88247327+shivendabhi@users.noreply.github.com>
2024-10-18 20:38:24 -0400

    Update route.ts

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,13 +1,20 @@
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
-import {appRouter} from "@/trpc"
+import { appRouter } from "@/trpc"
+import { db } from '@/db'
 
-function handler(req: Request) {
-  return fetchRequestHandler({
-    endpoint: '/api/trpc',
-    req,
-    router: appRouter,
-    createContext: () => ({})
-  });
+async function handler(req: Request) {
+  try {
+    await db.$connect()
+    return fetchRequestHandler({
+      endpoint: '/api/trpc',
+      req,
+      router: appRouter,
+      createContext: () => ({})
+    });
+  } catch (error) {
+    console.error('Error in tRPC handler:', error);
+    return new Response('Internal Server Error', { status: 500 });
+  }
 }
 
 export { handler as GET, handler as POST };
